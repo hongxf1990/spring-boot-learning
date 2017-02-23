c2f5299300827e9a462d108b7356737db1810ad9
hongxf1990 <hongxf19900420@gmail.com>
2017-02-23 11:12:48 +0800

    URL的匹配规则 解决406错误

--- src/main/java/com/petter/config/WebDemoConfig.java
+++ src/main/java/com/petter/config/WebDemoConfig.java
@@ -4,7 +4,9 @@ import com.petter.servlet.ValidateCodeServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -40,5 +42,37 @@ public class WebDemoConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ServletRegistrationBean validateCodeServlet() {
         return new ServletRegistrationBean(new ValidateCodeServlet(), "/validateCodeServlet");
+    }
+
+    /**
+     * setUseSuffixPatternMatch(boolean useSuffixPatternMatch)：
+     * 设置是否是后缀模式匹配，如“/user”是否匹配/user.*，默认真即匹配；
+     * 当此参数设置为true的时候，那么/user.html，/user.aa，/user.*都能是正常访问的。
+     * 当此参数设置为false的时候，那么只能访问/user
+     *
+     * setUseTrailingSlashMatch(boolean useSuffixPatternMatch)：
+     * 设置是否自动后缀路径模式匹配，如“/user”是否匹配“/user/”，默认真即匹配；
+     * 当此参数设置为true的后，那么地址/user，/user/都能正常访问。
+     * 当此参数设置为false的时候，那么就只能访问/user了。
+     *
+     * @param configurer
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(true);
+    }
+
+    /**
+     * (406)Could not find acceptable representation原因及解决方法
+     * 比如通过/login.html请求/login本应该返回json数据，系统会根据html去找html，类型不匹配报错
+     * favorPathExtension表示支持后缀匹配，
+     * 属性ignoreAcceptHeader默认为false，表示accept-header匹配
+     * defaultContentType开启默认匹配。
+     * 例如：请求aaa.xx，若设置<entry key="xx" value="application/xml"/> 也能匹配以xml返回。
+     * 根据以上条件进行一一匹配最终，得到相关并符合的策略初始化ContentNegotiationManager
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
     }
 }
