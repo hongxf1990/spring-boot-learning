2bfb72b36d91bf9f63e74177992ff2ce37b5d991
hongxf1990 <hongxf19900420@gmail.com>
2017-06-01 14:13:30 +0800

    定时任务动态修改cron参数

--- src/main/java/com/petter/scheduling/SchedulingConfig.java
+++ src/main/java/com/petter/scheduling/SchedulingConfig.java
@@ -2,6 +2,8 @@ package com.petter.scheduling;
 
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
+
 /**
  * @author Administrator
  * @since 2017-02-12 0:21
@@ -10,8 +12,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 //@EnableScheduling
 public class SchedulingConfig {
 
+    /**
+     * 我们希望这个方法每20秒打印一次.
+     * cron: 定时任务表达式.
+     *
+     * 指定：秒，分钟，小时，日期，月份，星期，年（可选）.
+     * *：任意.
+     *
+     */
     @Scheduled(cron = "0/20 * * * * ?") // 每20秒执行一次
     public void scheduler() {
         System.out.println(">>>>>>>>> SchedulingConfig.scheduler()");
+    }
+
+    /**
+     * 我们希望这个方法每1分钟打印一次.
+     */
+    @Scheduled(cron="0 0/1 * * * *")
+    public void tast2(){
+        System.out.println("MyTask.tast2()," + new Date());
     }
 }
---
+++ src/main/java/com/petter/scheduling/TaskCronChange.java
@@ -0,0 +1,67 @@
+package com.petter.scheduling;
+
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.TriggerContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 定时任务动态修改cron参数
+ * @author hongxf
+ * @since 2017-06-01 13:52
+ */
+@Component
+@EnableScheduling
+public class TaskCronChange implements SchedulingConfigurer {
+
+    private static String cron;
+
+    //外部修改
+    public static void setCron(String cron) {
+        TaskCronChange.cron = cron;
+    }
+
+    public TaskCronChange() {
+        //默认是：每5秒执行一次
+        cron = "0/5 * * * * *";
+        new Thread(() -> {
+            try {
+                //休眠15秒
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //修改为：每10秒执行一次
+            cron = "0/10 * * * * *";
+            System.err.println("cron change to:" + cron);
+        }).start();
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                //任务逻辑代码部分.
+                System.out.println("TaskCronChange task is running ... " + new Date());
+            }
+        };
+
+        Trigger trigger = new Trigger() {
+            @Override
+            public Date nextExecutionTime(TriggerContext triggerContext) {
+                //任务触发，可修改任务的执行周期.这里是用一个线程进行自动修改了
+                CronTrigger trigger = new CronTrigger(cron);
+                return trigger.nextExecutionTime(triggerContext);
+            }
+        };
+        //两个参数，一个Runnable,一个是Trigger，在Runnable中执行业务逻辑代码，在Trigger修改定时任务的执行周期。
+        taskRegistrar.addTriggerTask(task, trigger);
+    }
+
+}
