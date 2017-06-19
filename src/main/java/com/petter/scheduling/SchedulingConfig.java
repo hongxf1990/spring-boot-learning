9d65b898a7ee6ed93334f4b241ddbadbc25b6bfc
hongxf1990 <hongxf19900420@gmail.com>
2017-06-19 16:06:57 +0800

    spring自带的任务的一些配置

--- src/main/java/com/petter/scheduling/SchedulingConfig.java
+++ src/main/java/com/petter/scheduling/SchedulingConfig.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class SchedulingConfig {
 
     /**
-     * 我们希望这个方法每20秒打印一次.
+     * 这个方法每20秒打印一次.
      * cron: 定时任务表达式.
      *
      * 指定：秒，分钟，小时，日期，月份，星期，年（可选）.
@@ -29,7 +29,44 @@ public class SchedulingConfig {
      * 我们希望这个方法每1分钟打印一次.
      */
     @Scheduled(cron="0 0/1 * * * *")
-    public void tast2(){
+    public void task2(){
         System.out.println("MyTask.tast2()," + new Date());
+    }
+
+    /**
+     * 固定等待时间@Scheduled(fixedDelay = 时间间隔 )，不管线程任务的执行时间，每次都要把任务执行完成后再延迟固定时间后再执行下一次。
+     * ①程序启动的时候，会执行第一次；
+     * ②第二次执行的前提是上一次任务执行完毕之后才会执行。
+     * ③如果，间隔时间>=程序执行时间，每次时间差=间隔时间+程序执行时间}
+     * ④如果，间隔时间<程序执行时间，时间差可能是=程序执行时间（这是大部分的情况）；也可能是=间隔时间+程序执行时间；
+     */
+    @Scheduled(fixedDelay = 3000)
+    public void task3(){
+        Date d = new Date();
+        System.out.println("MyTask.task3()," + d);
+        try {
+            Thread.sleep(6000);
+            System.out.println("MyTask.task3(),"+ d +".end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 固定间隔时间@Scheduled(fixedRate = 时间间隔 )，是以固定频率来执行线程任务。
+     * (a)程序启动之后，就会启动第一次；
+     * (b)如果，间隔时间>=程序执行时间，每次时间差=间隔时间；
+     * (c)如果，间隔时间<程序执行时间，每次时间差=间隔时间+程序执行时间；
+     */
+    @Scheduled(fixedRate  = 3000)
+    public void task4(){
+        Date d = new Date();
+        System.out.println("MyTask.task4(),"+d);
+        try {
+            Thread.sleep(2000);
+            System.out.println("MyTask.task4(),"+d+".end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
